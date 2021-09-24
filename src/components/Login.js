f6fc83c51d8fb51ba89e5e4480c7725e3a6da4d9
Mateen Ahmed <mateen462@gmail.com>
2021-09-24 22:03:19 +0500

    Login Page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
+
+// css
+import "../assets/styles/Login.css";
+
+// Firebase
+import { auth } from "../server/firestore";
 
 const Login = () => {
+  const [loginPage, setLoginPage] = useState(true);
+
+  const [fullName, setFullName] = useState("");
+  const [profileURL, setProfileURL] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signUpHandler = async () => {
+    if (fullName !== "" && email !== "" && password !== "") {
+      try {
+        const userAuth = await auth.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+        // Save additional Info
+        userAuth.user.updateProfile({
+          fullName,
+          profileURL,
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
   return (
-    <div>
-      <h4>Login Page</h4>
+    <div className="login_container">
+      <img
+        src="https://1000logos.net/wp-content/uploads/2017/03/Linkedin-Logo.png"
+        alt="LinkedIn Clone"
+      />
+      {loginPage ? (
+        <>
+          <form>
+            <input type="email" placeholder="Enter Email" />
+            <input type="password" placeholder="Enter Password" />
+          </form>
+          <button>Login</button>
+          <p>
+            Are you a new user?{" "}
+            <span className="register_link" onClick={() => setLoginPage(false)}>
+              Register Now
+            </span>
+          </p>
+        </>
+      ) : (
+        <>
+          <form>
+            <input
+              type="text"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              placeholder="Enter Full Name"
+            />
+            <input
+              type="text"
+              value={profileURL}
+              onChange={(e) => setProfileURL(e.target.value)}
+              placeholder="Enter Profile URL"
+            />
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter Email"
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter Password"
+            />
+          </form>
+          <button onClick={signUpHandler}>Sign Up</button>
+          <p>
+            Already have an account?{" "}
+            <span className="register_link" onClick={() => setLoginPage(true)}>
+              Login
+            </span>
+          </p>
+        </>
+      )}
     </div>
   );
 };
